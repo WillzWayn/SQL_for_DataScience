30660ed67af7842faa6ef315627f518b06dc7d0a
Praneesh <khanna.praneesh@gmail.com>
2019-01-26 20:38:24 -0500

    mode sql2

--- sql_case.sql
+++ sql_case.sql
@@ -9,6 +9,22 @@ SELECT count(*),
             WHEN state = 'WA' THEN 'Washington West Coast'
        ELSE 'Other' END AS State_wise_dist
 FROM benn.college_football_players
-WHERE weight > 300
+WHERE weight >= 300
 GROUP BY 2
 ORDER BY 2; 
+
+
+/*
+Write a query that calculates the combined weight of all underclass players (FR/SO) in California
+as well as the combined weight of all upperclass players (JR/SR) in California.
+*/
+
+
+SELECT CASE
+           WHEN year IN ('FR', 'SO') THEN 'Underclass'
+           WHEN year IN ('JR', 'SR') THEN 'Upperclass'
+           ELSE NULL END AS class,
+           SUM(weight) as combined_weight
+FROM benn.college_football_players
+WHERE state = 'CA'
+GROUP BY 1;
