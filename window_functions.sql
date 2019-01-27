94995d9bb677b5d478589bcfbf7361541cb6c469
Praneesh <khanna.praneesh@gmail.com>
2019-01-27 01:54:37 -0500

    mode sql8

--- window_functions.sql
+++ window_functions.sql
@@ -24,3 +24,40 @@ SELECT  start_terminal,
         (duration_seconds/SUM(duration_seconds) OVER(partition by start_terminal))*100 as pcnt_total_time
 FROM tutorial.dc_bikeshare_q1_2012;
 
+/*
+Write a query that shows a running total of the duration of bike rides (similar to the last example),
+but grouped by end_terminal, and with ride duration sorted in descending order.
+*/
+
+
+SELECT end_terminal,
+       duration_seconds,
+       SUM(duration_seconds) OVER(PARTITION BY end_terminal ORDER BY duration_seconds DESC) as total_duration 
+FROM tutorial.dc_bikeshare_q1_2012
+WHERE start_time < '2012-01-08'
+
+
+
+/*
+Write a query that shows the 5 longest rides from each starting terminal, ordered by terminal,
+and longest to shortest rides within each terminal. Limit to rides that occurred before Jan. 8, 2012.
+*/
+
+SELECT t.top_5,
+       start_terminal,
+       duration_seconds        
+FROM
+(SELECT start_terminal,
+       duration_seconds,
+       RANK() OVER(PARTITION BY start_terminal ORDER BY duration_seconds DESC ) as top_5
+FROM tutorial.dc_bikeshare_q1_2012
+WHERE start_time < '2012-01-08') t
+WHERE t.top_5 <= 5
+
+
+
+
+
+
+
+
