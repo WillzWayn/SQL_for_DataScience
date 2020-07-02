d5083d10b4010f0f5a8881444fcefe3bba68de76
praneesh12 <khanna.praneesh@gmail.com>
2020-07-02 00:59:09 -0400

    leetcode/HARD/TournamentWinners(Hard).sql

---
+++ leetcode/HARD/TournamentWinners(Hard).sql
@@ -0,0 +1,116 @@
+Tournament Winners (Hard)
+Table: Players
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| player_id   | int   |
+| group_id    | int   |
++-------------+-------+
+player_id is the primary key of this table.
+Each row of this table indicates the group of each player.
+Table: Matches
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| match_id      | int     |
+| first_player  | int     |
+| second_player | int     | 
+| first_score   | int     |
+| second_score  | int     |
++---------------+---------+
+match_id is the primary key of this table.
+Each row is a record of a match, first_player and second_player contain the player_id of each match.
+first_score and second_score contain the number of points of the first_player and second_player respectively.
+You may assume that, in each match, players belongs to the same group.
+ 
+
+The winner in each group is the player who scored the maximum total points within the group. In the case of a tie, the lowest player_id wins.
+
+Write an SQL query to find the winner in each group.
+
+The query result format is in the following example:
+
+Players table:
++-----------+------------+
+| player_id | group_id   |
++-----------+------------+
+| 15        | 1          |
+| 25        | 1          |
+| 30        | 1          |
+| 45        | 1          |
+| 10        | 2          |
+| 35        | 2          |
+| 50        | 2          |
+| 20        | 3          |
+| 40        | 3          |
++-----------+------------+
+
+Matches table:
++------------+--------------+---------------+-------------+--------------+
+| match_id   | first_player | second_player | first_score | second_score |
++------------+--------------+---------------+-------------+--------------+
+| 1          | 15           | 45            | 3           | 0            |
+| 2          | 30           | 25            | 1           | 2            |
+| 3          | 30           | 15            | 2           | 0            |
+| 4          | 40           | 20            | 5           | 2            |
+| 5          | 35           | 50            | 1           | 1            |
++------------+--------------+---------------+-------------+--------------+
+
+Result table:
++-----------+------------+
+| group_id  | player_id  |
++-----------+------------+ 
+| 1         | 15         |
+| 2         | 35         |
+| 3         | 40         |
++-----------+------------+
+
+Create table If Not Exists Players (player_id int, group_id int);
+Create table If Not Exists Matches (match_id int, first_player int, second_player int, first_score int, second_score int)
+;
+Truncate table Players;
+insert into Players (player_id, group_id) values ('10', '2');
+insert into Players (player_id, group_id) values ('15', '1');
+insert into Players (player_id, group_id) values ('20', '3');
+insert into Players (player_id, group_id) values ('25', '1');
+insert into Players (player_id, group_id) values ('30', '1');
+insert into Players (player_id, group_id) values ('35', '2');
+insert into Players (player_id, group_id) values ('40', '3');
+insert into Players (player_id, group_id) values ('45', '1');
+insert into Players (player_id, group_id) values ('50', '2');
+Truncate table Matches;
+insert into Matches (match_id, first_player, second_player, first_score, second_score) values ('1', '15', '45', '3', '0');
+insert into Matches (match_id, first_player, second_player, first_score, second_score) values ('2', '30', '25', '1', '2');
+insert into Matches (match_id, first_player, second_player, first_score, second_score) values ('3', '30', '15', '2', '0');
+insert into Matches (match_id, first_player, second_player, first_score, second_score) values ('4', '40', '20', '5', '2');
+insert into Matches (match_id, first_player, second_player, first_score, second_score) values ('5', '35', '50', '1', '1');
+
+
+
+select group_id,
+       player_id
+from
+(
+  select group_id,
+         player_id,
+         sum(scores) as total_score
+  from Players p join
+  (
+    select first_player as player,
+      	   first_score as scores
+    from Matches
+    union all
+    select second_player as player,
+    	   second_score as scores
+    from Matches
+    ) t
+  on p.player_id = t.player
+  group by group_id
+  order by group_id , total_score
+) t1
+
+
+
+
